aa6c1607d8c156a95a1e354fcd9757b632420651
Siddhartha Chatterjee <35187839+sidinsomniac@users.noreply.github.com>
2020-10-25 20:49:17 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const year = new Date().getFullYear();
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>Copyright {year}</p>
+    </footer>
+  );
+}
