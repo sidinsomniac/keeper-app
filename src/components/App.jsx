92863a7fd87e0d2bfa13840bb64b6476d468b7e7
Siddhartha Chatterjee <35187839+sidinsomniac@users.noreply.github.com>
2020-10-26 19:55:22 +0530

    Map notes and pass info through props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 export default function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((note) => (
+        <Note key={note.id} title={note.title} content={note.content} />
+      ))}
       <Footer />
     </div>
   );
